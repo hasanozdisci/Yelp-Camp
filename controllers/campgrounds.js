b519e8bd32890a847b9e32f3da22811c27183592
Hasan Özdişçi <hasanozdisci@gmail.com>
2022-09-12 17:33:30 +0300

    adding maps

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,8 @@
 const Campground = require("../models/campground");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+const { cloudinary } = require("../cloudinary");
 
 // ALL CAMPGROUNDS
 module.exports.index = async (req, res) => {
@@ -13,19 +17,27 @@ module.exports.renderNewForm = (req, res) => {
 
 // CREATE NEW CAMPGROUND
 module.exports.createCampground = async (req, res, next) => {
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
+  res.send(geoData.body.features[0].geometry.coordinates);
+  // res.send("OK!!")
   // if (!req.body.campground)
   //   throw new ExpressError("Invalid Campground Data", 400);
-  const campground = new Campground(req.body.campground);
-  campground.images = req.files.map((f) => ({
-    url: f.path,
-    filename: f.filename,
-  }));
-  campground.author = req.user._id;
-  // console.log(campground.author)
-  await campground.save();
-  console.log(campground);
-  req.flash("success", "Successfully made a new campground!");
-  res.redirect(`/campgrounds/${campground._id}`);
+  // const campground = new Campground(req.body.campground);
+  // campground.images = req.files.map((f) => ({
+  //   url: f.path,
+  //   filename: f.filename,
+  // }));
+  // campground.author = req.user._id;
+  // // console.log(campground.author)
+  // await campground.save();
+  // console.log(campground);
+  // req.flash("success", "Successfully made a new campground!");
+  // res.redirect(`/campgrounds/${campground._id}`);
 };
 
 // SHOW PAGE
@@ -60,13 +72,24 @@ module.exports.renderEditForm = async (req, res) => {
 // UPDATE CAMPGROUND
 module.exports.updateCampground = async (req, res) => {
   const { id } = req.params;
-  console.log(req.body)
+  // console.log(req.body)
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
   const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
   campground.images.push(...imgs);
   await campground.save();
+  // delete images in edit page
+  if (req.body.deleteImages) {
+    // delete images from cloudinary
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
+    }
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+    // console.log(campground)
+  }
   req.flash("success", "Succesfully uptated campground");
   res.redirect(`/campgrounds/${campground._id}`);
 };
